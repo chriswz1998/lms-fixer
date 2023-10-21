ae3c71f3d5a8c53e3bbd1c0eafec3df8ae234def
chriswz1998 <scissorwz@gmail.com>
2023-10-20 22:29:47 -0700

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+      <div
+          className={ 'absolute w-full h-full flex items-center justify-center' }>
+        { children }
+      </div>
+  )
+}
+
+export default AuthLayout
